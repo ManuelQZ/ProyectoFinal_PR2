9e9139f4a95cd3df77f43a586550f4eb57b9f36a
ManuelQZ <142362671+ManuelQZ@users.noreply.github.com>
2024-04-14 23:09:15 -0500

    Subida de fxml y cambios
    Se realiza el cambio de FXML y se crean los controladores

--- src/main/java/co/edu/uniquindio/parcial1fx/proyecto/ViewController/MainController.java
+++ src/main/java/co/edu/uniquindio/parcial1fx/proyecto/ViewController/MainController.java
@@ -2,12 +2,11 @@ package co.edu.uniquindio.parcial1fx.proyecto.ViewController;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
 
 public class MainController {
 
@@ -18,33 +17,16 @@ public class MainController {
     private URL location;
 
     @FXML
-    private ImageView imageViewLogo;
-
-    @FXML
-    private TableView<?> tableViewVehiculoTransporte;
-
-    @FXML
-    private TableColumn<?, ?> tbCorreo;
-
-    @FXML
-    private TableColumn<?, ?> tbNombre;
-
-    @FXML
-    private TableColumn<?, ?> tbSaldo;
-
-    @FXML
-    private TextField txtCorreo;
-
-    @FXML
-    private TextField txtNombre;
+    private TableView<?> tableViewUsuario;
 
     @FXML
     private TextField txtSaldo;
 
     @FXML
-    void UpdateUsuario(ActionEvent event) {
+    private TextField txtCorreo;
 
-    }
+    @FXML
+    private TextField txtnombre;
 
     @FXML
     void addUsuario(ActionEvent event) {
@@ -57,16 +39,16 @@ public class MainController {
     }
 
     @FXML
-    void initialize() {
-        assert imageViewLogo != null : "fx:id=\"imageViewLogo\" was not injected: check your FXML file 'main.fxml'.";
-        assert tableViewVehiculoTransporte != null : "fx:id=\"tableViewVehiculoTransporte\" was not injected: check your FXML file 'main.fxml'.";
-        assert tbCorreo != null : "fx:id=\"tbCorreo\" was not injected: check your FXML file 'main.fxml'.";
-        assert tbNombre != null : "fx:id=\"tbNombre\" was not injected: check your FXML file 'main.fxml'.";
-        assert tbSaldo != null : "fx:id=\"tbSaldo\" was not injected: check your FXML file 'main.fxml'.";
-        assert txtCorreo != null : "fx:id=\"txtCorreo\" was not injected: check your FXML file 'main.fxml'.";
-        assert txtNombre != null : "fx:id=\"txtNombre\" was not injected: check your FXML file 'main.fxml'.";
-        assert txtSaldo != null : "fx:id=\"txtSaldo\" was not injected: check your FXML file 'main.fxml'.";
+    void updateUsuario(ActionEvent event) {
 
     }
 
+    @FXML
+    void initialize() {
+        assert tableViewUsuario != null : "fx:id=\"tableViewUsuario\" was not injected: check your FXML file 'main.fxml'.";
+        assert txtSaldo != null : "fx:id=\"txtSaldo\" was not injected: check your FXML file 'main.fxml'.";
+        assert txtCorreo != null : "fx:id=\"txtCorreo\" was not injected: check your FXML file 'main.fxml'.";
+        assert txtnombre != null : "fx:id=\"txtnombre\" was not injected: check your FXML file 'main.fxml'.";
+
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module co.edu.uniquindio.parcial1fx.proyecto {
     requires javafx.fxml;
 
 
-    opens co.edu.uniquindio.parcial1fx.proyecto to javafx.fxml;
+    opens co.edu.uniquindio.parcial1fx.proyecto.ViewController to javafx.fxml;
     exports co.edu.uniquindio.parcial1fx.proyecto;
+    exports co.edu.uniquindio.parcial1fx.proyecto.ViewController;
 }
