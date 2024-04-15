50f6d647244cb8109c25336893b34889a0808a93
ManuelQZ <142362671+ManuelQZ@users.noreply.github.com>
2024-04-14 20:43:13 -0500

    Creación Model Factory

---
+++ src/main/java/co/edu/uniquindio/parcial1fx/proyecto/Factory/ModelFactory.java
@@ -0,0 +1,67 @@
+package co.edu.uniquindio.parcial1fx.proyecto.Factory;
+
+Public class ModelFactory(){
+
+    private static ModelFactory modelFactory;
+
+    private PrestamoObjeto prestamoObjeto;
+
+
+    private ModelFactory(){
+        prestamoObjeto = new PrestamoObjeto();
+        inicializarDatos();
+    }
+
+    public static ModelFactory getInstancia() {
+        if(modelFactory == null) {
+            modelFactory = new ModelFactory();
+        }
+        return modelFactory;
+    }
+
+    private void inicializarDatos() {
+        Cliente cliente1 = Cliente.builder()
+                .cedula("1094")
+                .nombre("juan")
+                .apellido("arias")
+                .direccion("armenia")
+                .edad(17)
+                .build();
+
+        Cliente cliente2 = Cliente.builder()
+                .cedula("1095")
+                .nombre("Ana")
+                .apellido("cardenas")
+                .direccion("quimbaya")
+                .edad(25)
+                .build();
+
+        Cliente cliente3 = Cliente.builder()
+                .cedula("1096")
+                .nombre("Pedro")
+                .apellido("perez")
+                .direccion("armenia")
+                .edad(40)
+                .build();
+
+        prestamoObjeto.getListaClientes().add(cliente1);
+        prestamoObjeto.getListaClientes().add(cliente2);
+        prestamoObjeto.getListaClientes().add(cliente3);
+    }
+
+
+    public boolean crearCliente(String cedula,
+                                String nombre,
+                                String apellido,
+                                String email,
+                                String telefonoFijo,
+                                String telefonoCelular,
+                                String direccion){
+        return prestamoObjeto.crearCliente(cedula,nombre,apellido,email,telefonoFijo,telefonoCelular,direccion);
+    }
+
+    public String obtenerClientesPorCiudad(String ciudad) {
+        return prestamoObjeto.obtenerClientesPorCiudad(ciudad);
+    }
+    }
+}
